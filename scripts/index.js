dbc9d4243096b3a965bc30ee0cb849484e5011b9
Lucas <regazzi58@gmail.com>
2021-01-22 12:38:44 -0500

    new webby

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('wave')
+ScrollReveal().reveal('header h1', { delay: 500 })
+ScrollReveal().reveal('header h2', { delay: 600 })
+ScrollReveal().reveal('main', { delay: 2000 })
+ScrollReveal().reveal('p.contact', { delay: 2100 })
