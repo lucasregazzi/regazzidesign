57d0c2421a1c9c133ced51dfe0af6dd1bb0b6cc8
Lucas <regazzi58@gmail.com>
2021-01-22 16:35:00 -0500

    good for now

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,25 @@
-ScrollReveal().reveal('wave')
-ScrollReveal().reveal('header h1', { delay: 500 })
-ScrollReveal().reveal('header h2', { delay: 600 })
-ScrollReveal().reveal('main', { delay: 2000 })
-ScrollReveal().reveal('p.contact', { delay: 2100 })
-ScrollReveal().reveal('footer li', { delay: 2100 })
+regazziDesign = {}
+
+regazziDesign.buttonClick = () => {
+    $('button.toggle').click(function(){
+        let toggleButton = $(this);
+        let access = toggleButton[Object.keys(toggleButton)[0]];
+        let parentElement = access.offsetParent;
+        $(parentElement).toggleClass('expand');
+        $('main ul li').not(parentElement).toggle();
+        $(toggleButton).toggleClass('expanded');
+        if ($(toggleButton).hasClass('expanded')) {
+            toggleButton.html('Less')
+        } else if (!$(toggleButton).hasClass('expanded')) {
+            toggleButton.html('More')
+        }
+    })
+}
+
+regazziDesign.init = () => {
+    regazziDesign.buttonClick();
+}
+
+$(function () {
+    regazziDesign.init();
+});
