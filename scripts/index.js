83716d6a6c4f7fd8c234a8ed7e94efd8008b8266
Lucas <regazzi58@gmail.com>
2021-01-29 17:01:04 -0500

    accessibility sr class update

--- scripts/index.js
+++ scripts/index.js
@@ -31,9 +31,31 @@ regazziDesign.fadeInOnLoad = () => {
         })
 }
 
+regazziDesign.linkInNewTabAccessibility = () => {
+    function addNoOpener(link) {
+        let linkTypes = (link.getAttribute('rel') || '').split(' ');
+        if (!linkTypes.includes('noopener')) {
+            linkTypes.push('noopener');
+        }
+        link.setAttribute('rel', linkTypes.join(' ').trim());
+    }
+
+    function addNewTabMessage(link) {
+        if (!link.querySelector('.screen-reader-only')) {
+            link.insertAdjacentHTML('beforeend', '<span class="srOnly">(opens in a new tab)</span>');
+        }
+    }
+
+    document.querySelectorAll('a[target="_blank"]').forEach(link => {
+        addNoOpener(link);
+        addNewTabMessage(link);
+    });
+}
+
 regazziDesign.init = () => {
     regazziDesign.fadeInOnLoad();
     regazziDesign.buttonClick();
+    regazziDesign.linkInNewTabAccessibility();
 }
 
 $(function () {
